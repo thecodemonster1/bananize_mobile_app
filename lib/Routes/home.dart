e7054a097d5bf120f999e44261add6e91e6599ae
theCodeMonster1 <amharmohamed@icloud.com>
2024-11-11 01:37:50 +0530

    some updated commits

---
+++ lib/Routes/home.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.amberAccent,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bananize_mobile_app/Routes/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: MyHome());
   }
 }
