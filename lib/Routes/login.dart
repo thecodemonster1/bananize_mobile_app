3c45072b68cb0814488b9b4f248acb4e99a5b017
theCodeMonster1 <amharmohamed@icloud.com>
2024-11-11 07:11:15 +0530

    created login and signup pages

---
+++ lib/Routes/login.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
