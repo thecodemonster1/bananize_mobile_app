54be78889eedf3d4ba1cca06e571ab3778d3e02a
theCodeMonster1 <amharmohamed@icloud.com>
2024-11-28 00:02:06 +0530

    feat: comment out Firebase options initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bananize_mobile_app/Routes/Pages/login.dart';
-import 'package:bananize_mobile_app/Routes/Services/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +6,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    // options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MainApp());
 }
