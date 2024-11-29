c581a16a7d34c83eee21c8d0dd8669bbe30a4b4d
theCodeMonster1 <amharmohamed@icloud.com>
2024-11-30 01:04:39 +0530

    feat: implement Apple Sign-In service

---
+++ lib/Routes/Services/apple_auth.dart
@@ -0,0 +1,15 @@
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<User?> signInWithApple() async {
+  final credential = await SignInWithApple.getAppleIDCredential(
+    scopes: [AppleIDAuthorizationScopes.email, AppleIDAuthorizationScopes.fullName],
+  );
+
+  final firebaseCredential = OAuthProvider("apple.com").credential(
+    idToken: credential.identityToken,
+    accessToken: credential.authorizationCode,
+  );
+
+  return (await FirebaseAuth.instance.signInWithCredential(firebaseCredential)).user;
+}
