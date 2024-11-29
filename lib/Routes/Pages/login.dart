2cbacff9bd41208c00d77a72f84cad7c262a0313
theCodeMonster1 <amharmohamed@icloud.com>
2024-11-30 00:37:32 +0530

    feat: implement Google Sign-In functionality in login page

--- lib/Routes/Pages/login.dart
+++ lib/Routes/Pages/login.dart
@@ -3,6 +3,7 @@ import "package:bananize_mobile_app/Routes/Pages/signup.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
+import "package:google_sign_in/google_sign_in.dart";
 
 class MyLogin extends StatelessWidget {
   MyLogin({super.key});
@@ -13,6 +14,7 @@ class MyLogin extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   void _login(BuildContext context) async {
     try {
@@ -35,6 +37,42 @@ class MyLogin extends StatelessWidget {
       // Show an error message if login fails
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Error: ${e.message ?? 'Login failed'}")),
+      );
+    }
+  }
+
+  Future<void> _signInWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        // The user canceled the sign-in
+        return;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+                'Sign in successful \nWelcome ${userCredential.user!.email}')),
+      );
+      // Navigate to the next screen if sign-in is successful
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MyRules()),
+      );
+    } on FirebaseAuthException catch (e) {
+      // Show an error message if sign-in fails
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.message ?? 'Sign in failed')),
       );
     }
   }
@@ -123,8 +161,9 @@ class MyLogin extends StatelessWidget {
                                 width: iconSize.toDouble(),
                                 height: iconSize.toDouble(),
                               ),
-                              onPressed:
-                                  () {}, // Add Google login functionality
+                              onPressed: () {
+                                _signInWithGoogle(context);
+                              }, // Google login functionality
                             ),
                             IconButton(
                               icon: Image.asset(
