c965d780b233e2650afaae6172bc51fa10cf0010
theCodeMonster1 <amharmohamed@icloud.com>
2024-11-30 01:04:48 +0530

    fix: correct indentation in main.dart for Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
-  );
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
   runApp(const MainApp());
 }
 
