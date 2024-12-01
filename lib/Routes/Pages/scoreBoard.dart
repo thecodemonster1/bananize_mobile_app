ec63c8bc4d318515cf5186e0dc910196fc138bf6
theCodeMonster1 <amharmohamed@icloud.com>
2024-12-01 12:07:17 +0530

    feat: add Scoreboard page with placeholder widget

---
+++ lib/Routes/Pages/scoreBoard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Scoreboard extends StatefulWidget {
+  const Scoreboard({super.key});
+
+  @override
+  State<Scoreboard> createState() => _ScoreboardState();
+}
+
+class _ScoreboardState extends State<Scoreboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
