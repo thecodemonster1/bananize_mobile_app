5d928d5a0fea4a44f8197c6e0ec06b42fcdf1468
theCodeMonster1 <amharmohamed@icloud.com>
2024-12-03 20:22:11 +0530

    feat: add Globals class for centralized color management

---
+++ lib/Routes/Widgets/globals.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Globals {
+  static Color bgColor1 = Color.fromARGB(255, 255, 255, 255);
+  static Color bgColor2 = Color.fromARGB(255, 255, 255, 255);
+}
