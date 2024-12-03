04ed68dbbfd8348a0e527113a2a1f6dd5f920540
theCodeMonster1 <amharmohamed@icloud.com>
2024-12-03 14:27:53 +0530

    feat: add email parameter to Gameover widget and log score on initialization

--- lib/Routes/Pages/gameOver.dart
+++ lib/Routes/Pages/gameOver.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class Gameover extends StatefulWidget {
   final int score;
-  const Gameover({super.key, required this.score});
+  final Set<String?> email;
+  const Gameover({super.key, required this.score, required this.email});
 
   @override
   State<Gameover> createState() => _GameoverState();
@@ -13,6 +14,7 @@ class _GameoverState extends State<Gameover> {
   void initState() {
     super.initState();
     // Perform your on-start action here
+    debugPrint("${widget.email}!! \nfinal score: ${widget.score}");
   }
 
   @override
@@ -28,3 +30,4 @@ class _GameoverState extends State<Gameover> {
     );
   }
 }
+
