b22db61d96668253a730dbdb1303ab3f5fcd9b72
theCodeMonster1 <amharmohamed@icloud.com>
2024-12-03 20:22:21 +0530

    refactor: enhance performance of gameOver function and streamline UI transitions

--- lib/Routes/Pages/login.dart
+++ lib/Routes/Pages/login.dart
@@ -2,6 +2,7 @@ import "package:bananize_mobile_app/Routes/Pages/rules.dart";
 import "package:bananize_mobile_app/Routes/Pages/signup.dart";
 import "package:bananize_mobile_app/Routes/Services/apple_auth.dart";
 import "package:bananize_mobile_app/Routes/Services/google_auth.dart";
+import "package:bananize_mobile_app/Routes/Widgets/globals.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
@@ -90,159 +91,173 @@ class MyLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo
-                Container(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Image.asset(
-                    'lib/Assets/Images/banana-white.png',
-                    width: logoSize.toDouble(),
-                    height: logoSize.toDouble(),
-                  ),
-                ),
-                const Text(
-                  'Bananize',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Card for Login Form
-                Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        12.0), // Rounded corners for the card
-                  ),
-                  elevation: 8.0, // Shadow effect for the card
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        // Login title
-                        const Text(
-                          'L O G I N',
-                          style: TextStyle(
-                              fontSize: 34, fontWeight: FontWeight.normal),
-                        ),
-                        const SizedBox(height: 20),
-
-                        // Email TextField
-                        TextField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-
-                        // Password TextField
-                        TextField(
-                          controller: _passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-
-                        // External login options (Inside the Card)
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            IconButton(
-                              icon: Image.asset(
-                                'lib/Assets/Images/1657952440google-logo-png-transparent.png.webp',
-                                width: iconSize.toDouble(),
-                                height: iconSize.toDouble(),
-                              ),
-                              onPressed: () {
-                                // _signInWithGoogle(context);
-                                signInWithGoogle();
-                              }, // Google login functionality
-                            ),
-                            IconButton(
-                              icon: Image.asset(
-                                'lib/Assets/Images/976px-Apple_logo_black.svg.png',
-                                width: iconSize.toDouble(),
-                                height: iconSize.toDouble(),
-                              ),
-                              onPressed: () {
-                                signInWithApple();
-                              }, // Add Apple login functionality
-                            ),
-                            IconButton(
-                              icon: Image.asset(
-                                'lib/Assets/Images/Facebook_Logo_2023.png',
-                                width: iconSize.toDouble(),
-                                height: iconSize.toDouble(),
-                              ),
-                              onPressed: () {
-                                // signInWithFacebook();
-                              }, // Add Facebook login functionality
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-
-                        // Sign in button
-                        ElevatedButton(
-                          onPressed: () {
-                            _login(context);
-                          }, // Add sign in functionality
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: Colors.black, // Text color (white)
-                            shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(8.0), // Rounded corners
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 32.0, // Horizontal padding
-                              vertical: 12.0, // Vertical padding
-                            ),
-                          ),
-                          child: const Text('Sign In'),
-                        ),
-                        const SizedBox(height: 10),
-
-                        // Forgot password link
-                        TextButton(
-                          onPressed: () {}, // Add forgot password functionality
-                          child: const Text('Forgot Password?'),
-                        ),
-
-                        // New user link
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => MySignup()),
-                            );
-                          },
-                          child: const Text('New User? Sign Up'),
-                        ),
-                      ],
+      // backgroundColor: Colors.white,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        // padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Globals.bgColor1, Globals.bgColor2],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo
+                  Container(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Image.asset(
+                      'lib/Assets/Images/banana-white.png',
+                      width: logoSize.toDouble(),
+                      height: logoSize.toDouble(),
                     ),
                   ),
-                ),
-              ],
+                  const Text(
+                    'Bananize',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Card for Login Form
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                          12.0), // Rounded corners for the card
+                    ),
+                    elevation: 8.0, // Shadow effect for the card
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          // Login title
+                          const Text(
+                            'L O G I N',
+                            style: TextStyle(
+                                fontSize: 34, fontWeight: FontWeight.normal),
+                          ),
+                          const SizedBox(height: 20),
+
+                          // Email TextField
+                          TextField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+
+                          // Password TextField
+                          TextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+
+                          // External login options (Inside the Card)
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                icon: Image.asset(
+                                  'lib/Assets/Images/1657952440google-logo-png-transparent.png.webp',
+                                  width: iconSize.toDouble(),
+                                  height: iconSize.toDouble(),
+                                ),
+                                onPressed: () {
+                                  // _signInWithGoogle(context);
+                                  signInWithGoogle();
+                                }, // Google login functionality
+                              ),
+                              IconButton(
+                                icon: Image.asset(
+                                  'lib/Assets/Images/976px-Apple_logo_black.svg.png',
+                                  width: iconSize.toDouble(),
+                                  height: iconSize.toDouble(),
+                                ),
+                                onPressed: () {
+                                  signInWithApple();
+                                }, // Add Apple login functionality
+                              ),
+                              IconButton(
+                                icon: Image.asset(
+                                  'lib/Assets/Images/Facebook_Logo_2023.png',
+                                  width: iconSize.toDouble(),
+                                  height: iconSize.toDouble(),
+                                ),
+                                onPressed: () {
+                                  // signInWithFacebook();
+                                }, // Add Facebook login functionality
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+
+                          // Sign in button
+                          ElevatedButton(
+                            onPressed: () {
+                              _login(context);
+                            }, // Add sign in functionality
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.white,
+                              backgroundColor:
+                                  Colors.black, // Text color (white)
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    18.0), // Rounded corners
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 32.0, // Horizontal padding
+                                vertical: 12.0, // Vertical padding
+                              ),
+                            ),
+                            child: const Text('Sign In'),
+                          ),
+                          const SizedBox(height: 10),
+
+                          // Forgot password link
+                          TextButton(
+                            onPressed:
+                                () {}, // Add forgot password functionality
+                            child: const Text('Forgot Password?'),
+                          ),
+
+                          // New user link
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MySignup()),
+                              );
+                            },
+                            child: const Text('New User? Sign Up'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
