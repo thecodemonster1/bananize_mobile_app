9957e07b09150f2b4c00343f64921b8aaea90eb3
theCodeMonster1 <amharmohamed@icloud.com>
2024-12-04 15:57:51 +0530

    feat: update rules page with improved instructions and reintroduce app bar

--- lib/Routes/Pages/rules.dart
+++ lib/Routes/Pages/rules.dart
@@ -10,16 +10,16 @@ class MyRules extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Globals.bgColor1,
-      //   elevation: 0,
-      //   centerTitle: true,
-      // ),
-      // backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Globals.bgColor1,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        padding: const EdgeInsets.only(top: 60),
+        // padding: const EdgeInsets.only(top: 60),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [Globals.bgColor1, Globals.bgColor2],
@@ -51,22 +51,21 @@ class MyRules extends StatelessWidget {
                         text:
                             "1. Solve math problems from images to earn points.\n\n"
                             "2. Earn 10 points for each correct answer.\n\n"
-                            "3. Activate Combo Mode by answering 3 questions in a row for 20 points per correct answer.\n\n"
-                            "4. You have 20 seconds to solve each question; failing costs 1 life.\n\n"
+                            "3. Activate Combo Mode by answering 3 questions consecutively for 20 points per correct answer.\n\n"
+                            "4. You have 25 seconds to solve each question; failing to answer costs 1 life.\n\n"
                             "5. Start with 5 lives; the game ends when all lives are lost.\n\n"
                             "6. Reach 100 points to level up, reset lives, and unlock new challenges.\n\n"
-                            "7. Pause and resume the timer at any time.\n\n"
-                            "8. Aim for combos, climb levels, and achieve the highest score! 🎉\n\n"
+                            "7. Pause and resume the timer anytime for strategic breaks.\n\n"
+                            "8. Maximize your score by maintaining streaks and leveraging Combo Mode.\n\n"
                             "9. Incorrect answers reset your combo streak and cost 1 life.\n\n"
-                            "10. Timer resets with each new question, so stay sharp!\n\n"
-                            "11. Lives are replenished when you level up, giving you a fresh start.\n\n"
-                            "12. Score higher by maintaining streaks and leveraging Combo Mode.\n\n"
-                            "13. New levels introduce increasing difficulty to test your skills.\n\n"
-                            "14. The game automatically moves to the next question after timeout.\n\n"
-                            "15. Keep an eye on the timer and think fast to beat the clock!\n\n"
-                            "16. Use the Pause button strategically to take a break and plan.\n\n"
-                            "17. Challenge yourself to break your previous high score. 🎉\n\n"
-                            "18. Remember, the ultimate goal is to have fun while sharpening your math skills! 🎯\n\n"
+                            "10. The timer resets with each new question, so stay sharp!\n\n"
+                            "11. Lives are replenished upon leveling up, offering a fresh start.\n\n"
+                            "12. Difficulty increases with each new level to test your skills.\n\n"
+                            "13. The game automatically advances to the next question after timeout.\n\n"
+                            "14. Manage the timer wisely and think quickly to beat the clock!\n\n"
+                            "15. Use the Skip button strategically to move on but lose 5 points.\n\n"
+                            "16. Push your limits to break your previous high score. 🎯\n\n"
+                            "17. Enjoy the game and sharpen your math skills along the way! 🎉\n\n"
                             "Good luck!",
                         style: const TextStyle(
                           fontSize: 18,
@@ -86,30 +85,6 @@ class MyRules extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // ElevatedButton(
-                  //   onPressed: () {
-                  //     Navigator.push(
-                  //       context,
-                  //       MaterialPageRoute(builder: (context) => Scoreboard()),
-                  //     );
-                  //   },
-                  //   style: ElevatedButton.styleFrom(
-                  //     foregroundColor: Colors.white,
-                  //     backgroundColor: Colors.black,
-                  //     shape: RoundedRectangleBorder(
-                  //       borderRadius: BorderRadius.circular(18.0),
-                  //     ),
-                  //     padding: const EdgeInsets.symmetric(
-                  //       horizontal: 32.0,
-                  //       vertical: 12.0,
-                  //     ),
-                  //   ),
-                  //   child: const Text(
-                  //     'Scoreboard',
-                  //     style:
-                  //         TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                  //   ),
-                  // ),
                   OutlinedButton.icon(
                     onPressed: () {
                       Navigator.push(
@@ -126,8 +101,8 @@ class MyRules extends StatelessWidget {
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 10),
                     ),
-                    icon: const Icon(Icons.exit_to_app,
-                        color: Color.fromARGB(179, 0, 0, 0), size: 28),
+                    // icon: const Icon(Icons.exit_to_app,
+                    //     color: Color.fromARGB(179, 0, 0, 0), size: 28),
                     label: const Text(
                       'Scoreboard',
                       style: TextStyle(
